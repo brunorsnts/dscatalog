4027e695a382e69299b2ef4e0e265b5791e7c9a0
brunorsnts <santos_2002@outlook.com.br>
2026-01-06 19:37:14 -0300

    Find Category by id, exception handling

---
+++ backend/src/main/java/com/brunosantos/dscatalog/resources/exceptions/StandardError.java
@@ -0,0 +1,22 @@
+package com.brunosantos.dscatalog.resources.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+
+@Getter
+@Setter
+public class StandardError implements Serializable {
+
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+    public StandardError() {
+    }
+}
--- backend/src/main/java/com/brunosantos/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/brunosantos/dscatalog/services/CategoryService.java
@@ -3,12 +3,13 @@ package com.brunosantos.dscatalog.services;
 import com.brunosantos.dscatalog.dto.CategoryDTO;
 import com.brunosantos.dscatalog.entities.Category;
 import com.brunosantos.dscatalog.repositories.CategoryRepository;
+import com.brunosantos.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,7 +20,15 @@ public class CategoryService {
 
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
-        List<Category> list = repository.findAll(;
+        List<Category> list = repository.findAll();
         return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+
     }
 }
